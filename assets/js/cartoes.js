d22dcd72a93373897851f601b8fe88059e7c1616
paulovitor20 <sherlok6934@gmail.com>
2026-08-26 11:44:36 -0300

    Adiciona ativacao e inativacao de cartoes estilo

--- assets/js/cartoes.js
+++ assets/js/cartoes.js
@@ -301,7 +301,7 @@ function renderizarTabela() {
 
                 </td>
 
-                <td>
+                <td class="acoes">
 
                     <!-- VISUALIZAR -->
                     <button
